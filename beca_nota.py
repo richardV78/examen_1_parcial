77b9c08c0890b9aa5e2f2251724978b2246bf9f6
richard <richardvalles94@gmail.com>
2024-02-24 09:45:24 -0600

    changes

---
+++ beca_nota.py
@@ -0,0 +1,13 @@
+#calcular si la nota del estudiante es o no apta para un beca
+print("Hola capital humano")
+nota_alumno = int(input("Dame tu promedio academico "))
+
+def beca(nota_alumno):
+    if nota_alumno >= 95:
+        print("FELICIDADES APLICAS PARA UNA BECA UNIVERSITARIA!!!")
+    else:
+        print("LO SENTIMOS TU PROMEDIO NO ES SUFICIENTE PARA UNA BECA")
+
+def noticia():
+    print(beca(nota_alumno))
+noticia()
