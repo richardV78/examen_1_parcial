4674f021896ec8cd0c3e033604bd45000be9de05
richard <richardvalles94@gmail.com>
2024-02-24 09:34:38 -0600

    changes

--- calc_descuento.py
+++ calc_descuento.py
@@ -1,10 +1,18 @@
 # 3er jercicio donde se pide calcular el 10 de descuento
 print("Hola, calculare tu descuento")
 
-
+print("Ingresa el total del precio de tus productos")
+print("Recibiras un 10 porciento de descuento si llevas mas de 500 cordobas")
+precio = int(input("dame la cantidad: "))
 
 def descuento(precio):
-    if precio >= 500
+    if precio > 500:
         descuent = precio * 0.10
-        descuent_final = precio + descuent
+        descuent_final = precio - descuent
+        print(f"tu descuento fue de  {descuent} y deberas pagar {descuent_final}")
+    else:
+        print("la cantidad no es suficiente para un descuento")
 
+
+print(descuento(precio)) 
+print("gracias")
