7a67de6bf1ac66baf566413fcac06b428b5a5941
richard <richardvalles94@gmail.com>
2024-02-24 09:22:08 -0600

    new_file

---
+++ calc_descuento.py
@@ -0,0 +1,10 @@
+# 3er jercicio donde se pide calcular el 10 de descuento
+print("Hola, calculare tu descuento")
+
+
+
+def descuento(precio):
+    if precio >= 500
+        descuent = precio * 0.10
+        descuent_final = precio + descuent
+
